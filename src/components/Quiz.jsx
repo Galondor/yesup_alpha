c32badb218da055827aa08c0f18de084d89da1fe
Galondor <johnyjoejoe55@gmail.com>
2023-09-21 14:09:09 -0400

    Responsiveness

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,32 +6,61 @@ function Quiz({ currentQuestion, optionClicked, showPersonalInfo }) {
     if (showPersonalInfo === true) {
         return (null);
     } else if (showPersonalInfo === false) {
+      if (window.innerWidth <= 425) {
         return (
-            <div className="quiz_container" id="quiz">
-            <div className="quiz_header">
-              <div className="loading_container">
-                <img src={loading} className="loader" alt="Loading..." />
-              </div>
-              <h2>
-                Question {currentQuestion + 1} out of {Questions.length}
-              </h2>
-              <h2 id="question">{Questions[currentQuestion].text}</h2>
-              <ul>
-                {Questions[currentQuestion].options.map((option) => {
-                  return (
-                    <li
-                      onClick={() => optionClicked(option.value)}
-                      className="option"
-                      key={option.id}
-                    >
-                      {option.text}
-                    </li>
-                  );
-                })}
-              </ul>
+          <div className="quiz_container" id="quiz">
+          <div className="quiz_header">
+            <div className="loading_container">
+              <img src={loading} className="loader" alt="Loading..." />
             </div>
+            <h2 className="question_text">
+              Question {currentQuestion + 1} / {Questions.length}
+            </h2>
+            <h2 id="question">{Questions[currentQuestion].text}</h2>
+            <ul>
+              {Questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    onClick={() => optionClicked(option.value)}
+                    className="option"
+                    key={option.id}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-        );
+        </div>
+      );
+      } else if (window.innerWidth > 425) {
+        return (
+          <div className="quiz_container" id="quiz">
+          <div className="quiz_header">
+            <div className="loading_container">
+              <img src={loading} className="loader" alt="Loading..." />
+            </div>
+            <h2 className="question_text">
+              Question {currentQuestion + 1} out of {Questions.length}
+            </h2>
+            <h2 id="question">{Questions[currentQuestion].text}</h2>
+            <ul>
+              {Questions[currentQuestion].options.map((option) => {
+                return (
+                  <li
+                    onClick={() => optionClicked(option.value)}
+                    className="option"
+                    key={option.id}
+                  >
+                    {option.text}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      );
+      }
     }
 }
 
